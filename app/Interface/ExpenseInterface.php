ddd819da4829633aba911aeaa4dbb62fabb35f74
Arsyad Arthan N <arsyadevelop@gmail.com>
2025-05-02 10:50:05 +0700

    Update getAll method in ExpenseInterface signature
    Modified the getAll method to accept sourceAbleId and sourceAbleType parameters instead of branchId. This change allows more flexible querying based on different source types.

--- app/Interface/ExpenseInterface.php
+++ app/Interface/ExpenseInterface.php
@@ -4,7 +4,7 @@ namespace App\Interface;
 
 interface ExpenseInterface
 {
-    public function getAll($branchId = null);
+    public function getAll($sourceAbleId = null, $sourceAbleType = null);
     public function getById(int $id);
     public function store(array $data);
     public function update(int $id, array $data);
